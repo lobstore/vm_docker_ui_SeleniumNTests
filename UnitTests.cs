d98719c711463cdfc8e36896860ca2a7c065159d
lobstore <63562913+lobstore@users.noreply.github.com>
2024-09-19 11:44:25 +0300

    Update UnitTests.cs

--- UnitTests.cs
+++ UnitTests.cs
@@ -1,46 +1,47 @@
-
-using System.Drawing;
-
+using System;
 namespace vm_docker_ui_NTests
 {
-    
     public class Tests
     {
-        ChromeOptions _chromeOptions;
-        string remote_url_driver_chrome = "http://localhost:4444/";
-        string homepage = "http://host.docker.internal:5000";
-        IWebDriver _webdriver;
+        private ChromeOptions _chromeOptions;
+        private string remoteUrlDriverChrome = "http://localhost:4444/";
+        private string homepage = "http://host.docker.internal:5000";
+        private IWebDriver _webdriver;
+        private WebDriverWait _wait;
+
         [SetUp]
         public void Setup()
         {
             _chromeOptions = new ChromeOptions();
-            _webdriver = new RemoteWebDriver(new Uri(remote_url_driver_chrome), _chromeOptions);
+            _webdriver = new RemoteWebDriver(new Uri(remoteUrlDriverChrome), _chromeOptions);
             _webdriver.Manage().Window.Maximize();
+            _wait = new WebDriverWait(_webdriver, TimeSpan.FromSeconds(10));
             _webdriver.Navigate().GoToUrl(homepage);
         }
-        
+
         [TestCase("example@gmail.com", "test1", "test1234")]
         [TestCase("example@yandex.ru", "test2", "test1234")]
         [TestCase("example@mail.ru", "test3", "test1234")]
         public void UserRegistrationSmokeTests(string email, string name, string password)
         {
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[3]");
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input", email);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input", name);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[3]/div/input", password);
+            ClickElementByXPath("/html/body/section/div[2]/div/div/div/form/button");
 
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[3]")).Click();
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input")).SendKeys(email);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input")).SendKeys(name);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[3]/div/input")).SendKeys(password);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/button")).Click();
-            Thread.Sleep(2000);
             _webdriver.Navigate().GoToUrl(homepage);
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]")).Click();
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input")).SendKeys(email);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input")).SendKeys(password);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/button")).Click();
-            Thread.Sleep(2000);
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input", email);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input", password);
+            ClickElementByXPath("/html/body/section/div[2]/div/div/div/form/button");
+
             _webdriver.Navigate().GoToUrl(homepage);
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]")).Click();
-            Assert.That(_webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/h1")).Text == $"Welcome, {name}!", "The user under the correct data was not created");
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
+            string welcomeText = GetElementTextByXPath("/html/body/section/div[2]/div/h1");
+            Assert.That(welcomeText == $"Welcome, {name}!", "The user under the correct data was not created");
         }
+
         [TestCase("", "test4", "")]
         [TestCase("example1@gmail.com", "test5", "")]
         [TestCase("example1", "test6", "test1234")]
@@ -48,23 +49,21 @@ namespace vm_docker_ui_NTests
         [TestCase("example1@gmail", "test8", "test1234")]
         public void UserRegistrationNegativeTests(string email, string name, string password)
         {
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[3]");
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input", email);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input", name);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[3]/div/input", password);
+            ClickElementByXPath("/html/body/section/div[2]/div/div/div/form/button");
 
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[3]")).Click();
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input")).SendKeys(email);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input")).SendKeys(name);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[3]/div/input")).SendKeys(password);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/button")).Click();
-            Thread.Sleep(2000);
             _webdriver.Navigate().GoToUrl(homepage);
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]")).Click();
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input")).SendKeys(email);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input")).SendKeys(password);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/button")).Click();
-            Thread.Sleep(2000);
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input", email);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input", password);
+            ClickElementByXPath("/html/body/section/div[2]/div/div/div/form/button");
+
             _webdriver.Navigate().GoToUrl(homepage);
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]")).Click();
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
             Assert.That(!_webdriver.PageSource.Contains("Profile") && !_webdriver.PageSource.Contains($"Welcome, {name}!"), "The user was created under incorrect data");
-  
         }
 
         [TestCase(360, 680)]
@@ -76,59 +75,61 @@ namespace vm_docker_ui_NTests
         [TestCase(1920, 1080)]
         public void ChangeWindowSizeHomePageElementsVisibilityTests(int width, int height)
         {
-            _webdriver.Manage().Window.Size = new Size(width, height);
+            _webdriver.Manage().Window.Size = new System.Drawing.Size(width, height);
             string bugtext = $"is not visible at {width}x{height}px";
-            IWebElement homebutton = _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[1]"));
-            IWebElement loginbutton = _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]"));
-            IWebElement registrationbutton = _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[3]"));
-            IWebElement welcometext = _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/h1"));
-            try
-            {
-                Assert.IsTrue(homebutton.Displayed, $"Home button {bugtext}");
-            }
-            catch (AssertionException)
-            { }
-            try
-            {
-                Assert.IsTrue(loginbutton.Displayed, $"Login button {bugtext}");
-            }
-            catch (AssertionException)
-            { }
-            try
-            {
-                Assert.IsTrue(registrationbutton.Displayed, $"Registration button {bugtext}");
-            }
-            catch (AssertionException)
-            { }
-            try
-            {
-                Assert.IsTrue(welcometext.Displayed, $"Welcome text {bugtext}");
-            }
-            catch (AssertionException)
-            { }
 
+            IWebElement homeButton = FindElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[1]");
+            IWebElement loginButton = FindElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
+            IWebElement registrationButton = FindElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[3]");
+            IWebElement welcomeText = FindElementByXPath("/html/body/section/div[2]/div/h1");
 
-            Thread.Sleep(2000);
+            Assert.IsTrue(homeButton.Displayed, $"Home button {bugtext}");
+            Assert.IsTrue(loginButton.Displayed, $"Login button {bugtext}");
+            Assert.IsTrue(registrationButton.Displayed, $"Registration button {bugtext}");
+            Assert.IsTrue(welcomeText.Displayed, $"Welcome text {bugtext}");
         }
 
         [TestCase("example2@gmail.com", "test9", "test1234")]
         public void RememberMeFeatureTest(string email, string name, string password)
         {
-            
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]")).Click();
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input")).SendKeys(email);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input")).SendKeys(password);
-            _webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/div/div/form/button")).Click();
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[1]/div/input", email);
+            SendKeysToElementByXPath("/html/body/section/div[2]/div/div/div/form/div[2]/div/input", password);
+            ClickElementByXPath("/html/body/section/div[2]/div/div/div/form/button");
+
             _webdriver.Quit();
+            Setup();
+
             _webdriver.Navigate().GoToUrl(homepage);
-            _webdriver.FindElement(By.XPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]")).Click();
-            Assert.That(_webdriver.FindElement(By.XPath("/html/body/section/div[2]/div/h1")).Text == $"Welcome, {name}!", "The \"Remember me\" function does not work after closing the browser");
+            ClickElementByXPath("//*[@id=\"navbarMenuHeroA\"]/div/a[2]");
+            string welcomeText = GetElementTextByXPath("/html/body/section/div[2]/div/h1");
+            Assert.That(welcomeText == $"Welcome, {name}!", "The \"Remember me\" function does not work after closing the browser");
         }
-        
+
         [TearDown]
         public void TearDown()
         {
             _webdriver.Quit();
+        }
+
+        private void ClickElementByXPath(string xPath)
+        {
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(xPath))).Click();
+        }
+
+        private void SendKeysToElementByXPath(string xPath, string text)
+        {
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xPath))).SendKeys(text);
+        }
+
+        private string GetElementTextByXPath(string xPath)
+        {
+            return _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xPath))).Text;
+        }
+
+        private IWebElement FindElementByXPath(string xPath)
+        {
+            return _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xPath)));
         }
     }
 }
